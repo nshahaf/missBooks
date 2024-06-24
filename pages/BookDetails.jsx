772b842a5e0d4d33e478c9cbbb568c3edb423778
nShahaf <shahafnoked@gmail.com>
2024-06-24 18:25:21 +0300

    added pageCount, publishDate and colored amount

--- pages/BookDetails.jsx
+++ pages/BookDetails.jsx
@@ -15,6 +15,32 @@ export function BookDetails() {
             .catch(err => console.log(err))
     }, [bookId])
 
+    //handle page count text
+    function getPageCount() {
+        let pageCount = book.pageCount
+        if (book.pageCount > 500) pageCount += " - Long reading"
+        else if (book.pageCount > 200) pageCount += " - Decent reading"
+        else if (book.pageCount < 100) pageCount += " - Light rading"
+        return pageCount
+    }
+
+    //handle publish Date text
+    function getPublisheDate() {
+        const currYear = new Date().getFullYear()
+        let publishedYear = book.publishedDate
+        let diff = currYear - publishedYear
+        if (diff > 10) publishedYear += " - Vintage"
+        else if (diff < 10) publishedYear += " - NEW!"
+        return publishedYear
+    }
+
+    //handle price class
+    function getPriceClass() {
+        if (book.listPrice.amount > 150) return "red"
+        else if (book.listPrice.amount < 20) return "green"
+        else return ""
+    }
+
     if (!book) return (<div>Loading...</div>)
 
     return (
@@ -24,11 +50,11 @@ export function BookDetails() {
             <img src={`../assets/img/${book.thumbnail}`} alt="" />
             <h5><span>Description: </span>{book.description}</h5>
             <h5><span>Authors: </span> {book.authors.join(' ')}</h5>
-            <h5><span>Published Date: </span> {book.publishedDate}</h5>
-            <h5><span>Page Count: </span> {book.pageCount}</h5>
+            <h5><span>Published Date: </span> {getPublisheDate()}</h5>
+            <h5><span>Page Count: </span> {getPageCount()}</h5>
             <h5><span>Categories: </span> {book.categories.join(' ')}</h5>
             <h5><span>Language: </span> {book.language}</h5>
-            <h5><span>List Price: </span> {book.listPrice.amount} {book.listPrice.currencyCode}</h5>
+            <h5 className={getPriceClass()}><span >List Price: </span> {book.listPrice.amount} {book.listPrice.currencyCode}</h5>
             <button ><Link to="/book">Back</Link></button>
         </div>
     )
