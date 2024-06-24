f02cb893e71f5f7a75281fc2d7309c0b03ec8727
nShahaf <shahafnoked@gmail.com>
2024-06-24 14:34:38 +0300

    add animate and react router libs + event bus service

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,3 +1,6 @@
+const Router = ReactRouterDom.HashRouter
+const { Route, Routes, Navigate } = ReactRouterDOM
+
 import { About } from "./pages/About.jsx";
 import { BookIndex } from "./pages/BookIndex.jsx";
 import { Home } from "./pages/Home.jsx";
